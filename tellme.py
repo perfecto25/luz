555a68e1d667709513847f181327b22fb9203537
mike.reider <mike.reider@bidstrading.com>
2018-01-29 17:18:44 -0500

    x

--- tellme.py
+++ tellme.py
@@ -1,11 +1,58 @@
 #!/usr/bin/python
 
 from __future__ import print_function
-import curses
-screen = curses.initscr()
 
-screen.border(0)
-screen.addstr(12, 25, "tellme")
-screen.refresh()
-screen.getch()
+
+from os import system
+import curses
+
+def get_param(prompt_string):
+    screen.clear()
+    screen.border(0)
+    screen.addstr(2, 2, prompt_string)
+    screen.refresh()
+    input = screen.getstr(10, 10, 60)
+    return input
+
+def execute_cmd(cmd_string):
+    system("clear")
+    a = system(cmd_string)
+    print("")
+    if a == 0:
+        print("Command executed correctly")
+    else:
+        print("Command terminated with error")
+    raw_input("Press enter")
+    print("")
+
+x = 0
+
+while x != ord('4'):
+    screen = curses.initscr()
+
+    screen.clear()
+    screen.border(0)
+    screen.addstr(2, 2, "Select Information type")
+    screen.addstr(4, 4, "1 - OS")
+    screen.addstr(5, 4, "2 - Network")
+    screen.addstr(6, 4, "3 - Disk")
+    screen.addstr(7, 4, "4 - Memory")
+    screen.refresh()
+
+    x = screen.getch()
+
+    if x == ord('1'):
+        username = get_param("Enter the username")
+        homedir = get_param("Enter the home directory, eg /home/nate")
+        groups = get_param("Enter comma-separated groups, eg adm,dialout,cdrom")
+        shell = get_param("Enter the shell, eg /bin/bash:")
+        curses.endwin()
+    # execute_cmd("useradd -d " + homedir + " -g 1000 -G " + groups + " -m -s " + shell + " " + username)
+    if x == ord('2'):
+        curses.endwin()
+        execute_cmd("apachectl restart")
+    if x == ord('3'):
+        curses.endwin()
+        execute_cmd("df -h")
+
 curses.endwin()
