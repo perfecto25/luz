180eca5594f8969787ce5c9916274b2e7ecbef87
perfecto25 <mike.reider@gmail.com>
2018-01-29 17:31:17 -0500

    2

--- tellme.py
+++ tellme.py
@@ -4,55 +4,31 @@ from __future__ import print_function
 
 
 from os import system
-import curses
 
-def get_param(prompt_string):
-    screen.clear()
-    screen.border(0)
-    screen.addstr(2, 2, prompt_string)
-    screen.refresh()
-    input = screen.getstr(10, 10, 60)
-    return input
+import npyscreen
+class TestApp(npyscreen.NPSApp):
+    def main(self):
+        # These lines create the form and populate it with widgets.
+        # A fairly complex screen in only 8 or so lines of code - a line for each control.
+        F  = npyscreen.Form(name = "Welcome to Npyscreen",)
+        t  = F.add(npyscreen.TitleText, name = "Text:",)
+        fn = F.add(npyscreen.TitleFilename, name = "Filename:")
+        fn2 = F.add(npyscreen.TitleFilenameCombo, name="Filename2:")
+        dt = F.add(npyscreen.TitleDateCombo, name = "Date:")
+        s  = F.add(npyscreen.TitleSlider, out_of=12, name = "Slider")
+        ml = F.add(npyscreen.MultiLineEdit,
+               value = """try typing here!\nMutiline text, press ^R to reformat.\n""",
+               max_height=5, rely=9)
+        ms = F.add(npyscreen.TitleSelectOne, max_height=4, value = [1,], name="Pick One",
+                values = ["Option1","Option2","Option3"], scroll_exit=True)
+        ms2= F.add(npyscreen.TitleMultiSelect, max_height =-2, value = [1,], name="Pick Several",
+                values = ["Option1","Option2","Option3"], scroll_exit=True)
 
-def execute_cmd(cmd_string):
-    system("clear")
-    a = system(cmd_string)
-    print("")
-    if a == 0:
-        print("Command executed correctly")
-    else:
-        print("Command terminated with error")
-    raw_input("Press enter")
-    print("")
+        # This lets the user interact with the Form.
+        F.edit()
 
-x = 0
+        print(ms.get_selected_objects())
 
-while x != ord('4'):
-    screen = curses.initscr()
-
-    screen.clear()
-    screen.border(0)
-    screen.addstr(2, 2, "Select Information type")
-    screen.addstr(4, 4, "1 - OS")
-    screen.addstr(5, 4, "2 - Network")
-    screen.addstr(6, 4, "3 - Disk")
-    screen.addstr(7, 4, "4 - Memory")
-    screen.refresh()
-
-    x = screen.getch()
-
-    if x == ord('1'):
-        username = get_param("Enter the username")
-        homedir = get_param("Enter the home directory, eg /home/nate")
-        groups = get_param("Enter comma-separated groups, eg adm,dialout,cdrom")
-        shell = get_param("Enter the shell, eg /bin/bash:")
-        curses.endwin()
-    # execute_cmd("useradd -d " + homedir + " -g 1000 -G " + groups + " -m -s " + shell + " " + username)
-    if x == ord('2'):
-        curses.endwin()
-        execute_cmd("apachectl restart")
-    if x == ord('3'):
-        curses.endwin()
-        execute_cmd("df -h")
-
-curses.endwin()
+if __name__ == "__main__":
+    App = TestApp()
+    App.run()
