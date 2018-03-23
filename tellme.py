693655da3f05dd9813f44a0aa53382fd256f3ce7
mrx <mike.reider@gmail.com>
2018-03-23 12:41:40 -0400

    Update tellme.py

--- tellme.py
+++ tellme.py
@@ -3,24 +3,16 @@
 
 from __future__ import print_function
 
-import npyscreen as np
-import curses
+import click
+import platform
+import psutil
+from huepy import *
 
-class ExitButton(np.ButtonPress):
-    def whenPressed(self):
-        self.parent.parentApp.switchForm(None)
+print(info(platform.machine()))
+print(cyan(platform.platform()))
+print(green(platform.uname()))
+print(info(psutil.disk_usage('/')))
 
-class TellMe(np.NPSApp):
-    def main(self):
-        F  = np.FormBaseNew(name = "tellme",)
-        os  = F.add(np.TitleText, name = "OS:", max_width=19)
-        network = F.add(np.TitleText, name = "Network:", max_width=19)
-        exit_btn = F.add(ExitButton, name="Exit", max_width=19)
-        display = F.add(np.BoxBasic, rely=1, relx=20)
-        F.edit()
-
-        print(ms.get_selected_objects())
-    
 
 if __name__ == "__main__":
     App = TellMe()
