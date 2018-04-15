0cdfaf72a5712987d99645472c1e587e6988b170
perfecto25 <mike.reider@gmail.com>
2018-04-15 13:35:24 -0400

    disk

--- luz/commands/disk.py
+++ luz/commands/disk.py
@@ -1,22 +1,50 @@
 import click
-import sh
-from sh import ErrorReturnCode
+import os
+import json
 from huepy import *
 # to do: 
 # windows OS
 # JSON return option
 
-def get_disk_space(path, json_flag):
-    try:
-        # if json_flag:
-        #     import pprint
-        #     return pprint.pprint({'path': path, 'space': sh.du('-sh', path)})
-        
-        return sh.du('-sh', path)
-        
-    except ErrorReturnCode as e:
-        return e.stdout
 
+def get_dir_size(json_flag, verbose, path='/'):
+    
+    total_size = 0
+
+    # JSON payload
+    payload = {}
+    payload['total'] = {}
+    payload['dirs'] = {}
+
+    for dirpath, dirnames, filenames in os.walk(path):
+        dirsize = 0
+
+        for f in filenames:
+            #print f
+            fp = os.path.join(dirpath, f)
+            try:
+                size = os.path.getsize(fp)
+            except OSError:
+                print('error %s' % f)
+                pass
+            #print("{0} {1}").format(size, fp)
+            #print(dirpath, dirnames, filenames,size)
+            dirsize += size
+           # print(dirpath)
+            total_size += size
+            print(fp)
+            payload['dirs'][fp] = dirsize
+    
+    # set total size for b, kb, mb, gb
+
+#    kb = "{0}".format(total_size, ",")
+    payload['total']['b'] = '{:,}'.format(total_size)
+    payload['total']['kb'] = '{:,}'.format(total_size/1000, ",")
+    payload['total']['mb'] = '{:,}'.format(total_size/1000000, ",")
+    payload['total']['gb'] = '{:,}'.format(total_size/1000000000, ",")
+
+    # return total payload data
+    return dict(payload)
 
 
 @click.group()
@@ -29,17 +57,40 @@ def disk():
 @click.command()
 @click.argument('path', default=None)
 @click.option('--json', is_flag=True)
-def size(path, json=None):
+@click.option('--verbose', '-v', multiple=True, is_flag=True, help="print all sub directories and their sizes")
+def size(path, json=False, verbose=False):
     '''
     check size of a given path or directory
     
     examples: \n\n
-        
-    luz disk size /tmp \n
-        
-    luz disk size /home --json (print output in JSON)
+      luz disk size /tmp \n 
+      luz disk size /home --json (print output in JSON) \n
+      luz disk size /home -v (print names and size of all subfolders)
+
     '''
-    click.echo('\n'+cyan('%s disk size\n' % path))
-    click.echo(yellow(get_disk_space(path, json)))
+    #click.echo('\n'+cyan('%s disk size\n' % path))
+
+    payload = get_dir_size(json, verbose, path)
+
+    if json:
+        if True in verbose:
+            print type(payload)
+            click.echo(yellow(payload))
+        else:
+            click.echo(yellow(json.dumps(payload['total'])))
+    else:
+        if True in verbose:
+            for d in payload['dirs']:
+                click.echo(yellow(str(d) + ':  ' + str(payload['dirs'][d]/1000) + ' kb'))
+            click.echo(yellow('total (b):  ' + str(payload['total']['b'])))
+            click.echo(yellow('total (kb):  ' + str(payload['total']['kb'])))
+            click.echo(yellow('total (mb):  ' + str(payload['total']['mb'])))
+            click.echo(yellow('total (gb):  ' + str(payload['total']['gb'])))
+        else:
+            click.echo(yellow('total:  ' + str(payload['total']['kb']) + ' kb'))
+
+        
+    
+    #click.echo(yellow(get_dir_size(json, verbose, path)))
 
 disk.add_command(size)
---
+++ luz/commands/test.py
@@ -0,0 +1,23 @@
+import os
+def get_dir_size(path=os.getcwd()):
+    
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(path):
+
+        dirsize = 0
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            try:
+                size = os.path.getsize(fp)
+            except OSError:
+                pass
+            #print("{0} {1}").format(size, fp)
+            #print(dirpath, dirnames, filenames,size)
+            dirsize += size
+            total_size += size
+        print(dirsize, dirpath)
+    print("{0} bytes".format(total_size))
+    print("{:,} KB").format(total_size/1000, ",")
+    print("{:,} MB").format(total_size/1000000)
+    print("{:,} GB").format(total_size/1000000000)
+print get_dir_size('/home/mrx')
